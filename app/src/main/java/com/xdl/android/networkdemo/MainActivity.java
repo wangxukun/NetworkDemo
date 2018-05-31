e76d61cce81b018658078ec9938de23b155151fb
wangxukun <wang_xukun@163.com>
2018-06-01 00:11:18 +0800

    Android实现网络下载 v1.0

---
+++ app/src/main/java/com/xdl/android/networkdemo/MainActivity.java
@@ -0,0 +1,89 @@
+package com.xdl.android.networkdemo;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements DownloadCallback<String>{
+
+    // Keep a reference to the NetworkFragment, which owns the AsyncTask object
+    // that is used to execute network ops.
+    private NetworkFragment mNetworkFragment;
+
+    // Boolean telling us whether a download is in progress, so we don't trigger overlapping
+    // downloads with consecutive button clicks.
+    private boolean mDownloading = false;
+    private Button mButton;
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNetworkFragment = NetworkFragment.getInstance(getSupportFragmentManager(), "https://github.com/wangxukun/pear/blob/master/src/wxk/bank/entity/User.java");
+
+        mButton = (Button)findViewById(R.id.download_button);
+        mButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startDownload();
+            }
+        });
+        mTextView = (TextView)findViewById(R.id.data_textView);
+    }
+
+    private void startDownload() {
+        if (!mDownloading && mNetworkFragment != null) {
+            // Execute the async download.
+            mNetworkFragment.startDownload();
+            mDownloading = true;
+        }
+    }
+
+    @Override
+    public void updateFromDownload(String result) {
+        // Update your UI here based on result of download.
+        mTextView.setText(result);
+    }
+
+    @Override
+    public NetworkInfo getActiveNetworkInfo() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo;
+    }
+
+    @Override
+    public void onProgressUpdate(int progressCode, int percentComplete) {
+        switch (progressCode) {
+            case Progress.ERROR:
+
+                break;
+            case Progress.CONNECT_SUCCESS:
+
+                break;
+            case Progress.GET_INPUT_STREAM_SUCCESS:
+
+                break;
+            case Progress.PROCESS_INPUT_STREAM_IN_PROGRESS:
+
+                break;
+            case Progress.PROCESS_INPUT_STREAM_SUCCESS:
+
+                break;
+        }
+    }
+
+    @Override
+    public void finishDownloading() {
+        mDownloading = false;
+        // TODO finishDownloading
+    }
+}
